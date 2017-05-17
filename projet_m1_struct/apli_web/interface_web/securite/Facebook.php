27a9bbfa056a7b567ee4d623c92c6506b2294f64
louisjaouen <louisjaouen@me.com>
2017-05-17 16:40:11 +0200

    debut config hybridauth

--- projet_m1_struct/apli_web/interface_web/securite/Facebook.php
+++ projet_m1_struct/apli_web/interface_web/securite/Facebook.php
@@ -1,22 +1,6 @@
 <?php
-//fb.php
-require_once '../../../vendor/autoload.php';
- 
-$config = array(
-    "base_url" => "92.92.96.14:80/serre/projet_m1_struct/interface_web/securite/hybrid.php",
-    "providers" => array(
-        "Facebook" => array(
-            "enabled" => true,
-            "keys" => array("id" => "446272929039156", "secret" => "0ec9951958695a404f7ea0b458ab5f83"),
-                    "scope" => "email"
-        )
-    )
-);
-$hybridAuth = new \Hybridauth\Hybridauth($config);
-$adapter = $hybridAuth->authenticate("Facebook");
-$userProfile = $adapter->getUserProfile();
-echo "<pre>";
-print_r($adapter->getTokens());
-print_r($userProfile);
-echo "</pre>";
-?>
+	$config_file_path = 'config.php';
+	 
+	require_once( "../../../vendor/hybridauth/hybridauth/Hybrid/Auth.php" );
+	 
+	$hybridauth = new Hybrid_Auth( $config_file_path );
--- projet_m1_struct/apli_web/interface_web/securite/hybrid.php
+++
@@ -1,6 +0,0 @@
-<?php
-//hybrid.php
-require_once '../../../vendor/autoload.php';
-$endPoint = new \Hybridauth\Endpoint();
-$endPoint->process();
-?>
