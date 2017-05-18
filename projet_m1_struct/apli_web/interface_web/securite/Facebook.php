d02aa58765cc05f0ddc3e991cb6cf2ba8b09eaf1
louisjaouen <louisjaouen@me.com>
2017-05-18 16:57:30 +0200

    retest configuration hybridauth

--- projet_m1_struct/apli_web/interface_web/securite/Facebook.php
+++ projet_m1_struct/apli_web/interface_web/securite/Facebook.php
@@ -33,8 +33,8 @@
                         <div id="my-signin" href="index.html"></div>
                         <div class="fb-login-button" data-max-rows="1" data-size="large" data-button-type="login_with" data-show-faces="true" data-auto-logout-link="true" data-use-continue-as="true" href="index.html"></div>
                         <h6>connectez vous avec :</h6>
-                        <a href="/login.php?provider=Google" title="Connexion Google">Google</a>
-                        <a href="/login.php?provider=facebook" title="Connexion Facebook">Facebook</a>
+                        <a href="http://serregoarem.ddnsking.com/serre/projet_m1_struct/apli_web/interface_web/securite/login.php?provider=Google" title="Connexion Google">Google</a>
+                        <a href="http://serregoarem.ddnsking.com/serre/projet_m1_struct/apli_web/interface_web/securite/login.php?provider=facebook" title="Connexion Facebook">Facebook</a>
                         <h2>ou</h2>
                         <form role="form">
                             <fieldset>
