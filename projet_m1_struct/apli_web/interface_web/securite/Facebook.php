467e59a228a02bf92e9ce2fa3f761cd9e92cdfb2
louisjaouen <louisjaouen@me.com>
2017-05-19 12:00:04 +0200

    debut ajout session

--- projet_m1_struct/apli_web/interface_web/securite/Facebook.php
+++ projet_m1_struct/apli_web/interface_web/securite/Facebook.php
@@ -1,3 +1,21 @@
+<?php
+// On prolonge la session
+session_start();
+// On teste si la variable de session existe et contient une valeur
+if(empty($_SESSION['login'])) 
+{
+  //echo "vous n'etes pas connecte";
+  // Si inexistante ou nulle, on redirige vers le formulaire de login
+  
+}else{
+  header('Location: http://serregoarem.ddnsking.com/serre/projet_m1_struct/apli_web/interface_web/html/tableau.php');
+  exit();
+}
+
+ ?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 
