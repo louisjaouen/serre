2a10bf674ac1727b14c3fee1f9ce538f4f4c5830
louisjaouen <louisjaouen@me.com>
2017-05-22 20:14:53 +0200

    modif dashboard

--- projet_m1_struct/apli_web/interface_web/html/tableau.php
+++ projet_m1_struct/apli_web/interface_web/html/tableau.php
@@ -126,11 +126,13 @@ if(empty($_SESSION['bassin']) || empty($_SESSION['capteur']) || empty($_SESSION[
             </div>
             <!-- /.navbar-static-side -->
         </nav>
+        <?php if($_SESSION['bassin']==1){
 
+        }?>
         <div id="page-wrapper">
             <div class="row">
                 <div class="col-lg-12">
-                    <h1 class="page-header">Dashboard</h1>
+                    <h1 class="page-header">Bassin <?php echo $_SESSION['bassin'];?></h1>
                 </div>
                 <!-- /.col-lg-12 -->
             </div>
@@ -164,6 +166,7 @@ if(empty($_SESSION['bassin']) || empty($_SESSION['capteur']) || empty($_SESSION[
            
         </div>
         <!-- /#page-wrapper -->
+        <?php else{}?>
 
     </div>
     <!-- /#wrapper -->
