9f4087a035ee79e2963e54c03a98936a8b414f09
louisjaouen <louisjaouen@me.com>
2017-05-22 17:49:44 +0200

    modif dashboard

--- projet_m1_struct/apli_web/interface_web/affichage_donnee/lecture_valeur_bdd.php
+++ projet_m1_struct/apli_web/interface_web/affichage_donnee/lecture_valeur_bdd.php
@@ -1,12 +1,8 @@
 <?php
 	header('Content-Type: text/html; charset: UTF-8');
-	//$bassin=$_GET['bassin'];
-	$capteur=$_GET['capteur'];
-	$nombre_de_valeur=$_GET['nombre_de_valeur'];
-
-
-
-
+	$bassin=$_GET['b'];
+	$capteur=$_GET['c'];
+	$nombre_de_valeur=$_GET['v'];
 	try{
         // Sous MAMP (Mac)
         $bdd = new PDO('mysql:host=localhost;dbname=projet;charset=utf8', 'root', 'Louloudu29');
@@ -15,27 +11,18 @@
 	{
 	    die('Erreur : ' . $e->getMessage());
 	}
-
-	
 	$sql = 'SELECT *' 
 		. ' FROM valeur' 
 		. ' WHERE id_capteur = ?'  
 		. ' ORDER BY date_valeur' 
 		. ' DESC LIMIT 0,?;';
 	$prep = $bdd->prepare($sql);
-
 	$prep->bindValue(1, intval($capteur,10), PDO::PARAM_INT);
 	$prep->bindValue(2, intval($nombre_de_valeur,10), PDO::PARAM_INT);
 	$prep->execute();
-
-
 	$str="";
 	$str .= '[';
 	$str .= '{"valeur": "';
-
-	
-
-
 	while($donnees= $prep->fetch()){
 	    $str .= $donnees['value'];
 	    $str .= '"},';
@@ -45,12 +32,6 @@
 	$str .= '"}';
 	$str.=']';
 	echo $str;
-
-
 	$rep->closeCursor();
 	$bdd=null;
-	
-
-
-
 ?>
