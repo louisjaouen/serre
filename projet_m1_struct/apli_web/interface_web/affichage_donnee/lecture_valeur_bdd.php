078af2a90e7bc575bae432b7235298f70cb0f1c0
louisjaouen <louisjaouen@me.com>
2017-05-22 16:40:58 +0200

    modif dashboard

--- projet_m1_struct/apli_web/interface_web/affichage_donnee/lecture_valeur_bdd.php
+++ projet_m1_struct/apli_web/interface_web/affichage_donnee/lecture_valeur_bdd.php
@@ -16,8 +16,8 @@
 	}
 
 	
-	$rep = $bdd->prepare('SELECT * FROM valeur WHERE id_capteur = :id_capteur  ORDER BY date_valeur DESC LIMIT 0, :nombre_de_valeur');
-	$rep->execute(array(':id_capteur' => $capteur,':nombre_de_valeur' => $nombre_de_valeur));
+	$rep = $bdd->prepare('SELECT * FROM valeur WHERE id_capteur = 2  ORDER BY date_valeur DESC LIMIT 0,10');
+	$rep->execute(array(':id_capteur' => $capteur, ':nombre_de_valeur' => $nombre_de_valeur));
 
 	$str="";
 	$str .= '[';
