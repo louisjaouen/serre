b9703adb9f569b322c382ab52bc709844eaeac4b
louisjaouen <louisjaouen@me.com>
2017-05-22 16:13:13 +0200

    modif dashboard

--- projet_m1_struct/apli_web/interface_web/affichage_donnee/js/miseajourcapteurlive.js
+++ projet_m1_struct/apli_web/interface_web/affichage_donnee/js/miseajourcapteurlive.js
@@ -1,3 +1,60 @@
+(function() {
+  /**
+   * Ajustement décimal d'un nombre
+   *
+   * @param {String}  type : Le type d'ajustement souhaité.
+   * @param {Number}  value : le nombre à traité The number.
+   * @param {Integer} exp  : l'exposant (le logarithme en base 10 de l'ajustement).
+   * @returns {Number} la valeur ajustée.
+   */
+  function decimalAdjust(type, value, exp) {
+    // Si la valeur de exp n'est pas définie ou vaut zéro...
+    if (typeof exp === 'undefined' || +exp === 0) {
+      return Math[type](value);
+    }
+    value = +value;
+    exp = +exp;
+    // Si la valeur n'est pas un nombre 
+    // ou si exp n'est pas un entier...
+    if (isNaN(value) || !(typeof exp === 'number' && exp % 1 === 0)) {
+      return NaN;
+    }
+    // Si la valeur est négative
+    if (value < 0) {
+      return -decimalAdjust(type, -value, exp);
+    }
+    // Décalage
+    value = value.toString().split('e');
+    value = Math[type](+(value[0] + 'e' + (value[1] ? (+value[1] - exp) : -exp)));
+    // Décalage inversé
+    value = value.toString().split('e');
+    return +(value[0] + 'e' + (value[1] ? (+value[1] + exp) : exp));
+  }
+
+  // Arrondi décimal
+  if (!Math.round10) {
+    Math.round10 = function(value, exp) {
+      return decimalAdjust('round', value, exp);
+    };
+  }
+  // Arrondi décimal inférieur
+  if (!Math.floor10) {
+    Math.floor10 = function(value, exp) {
+      return decimalAdjust('floor', value, exp);
+    };
+  }
+  // Arrondi décimal supérieur
+  if (!Math.ceil10) {
+    Math.ceil10 = function(value, exp) {
+      return decimalAdjust('ceil', value, exp);
+    };
+  }
+})();
+
+
+
+
+
 function createRequestObject()
 {
     var http;
