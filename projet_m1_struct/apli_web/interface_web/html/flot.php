95c6345edf51bb1ae1142b5cda57744363d735e1
louisjaouen <louisjaouen@me.com>
2017-05-23 11:08:14 +0200

    modif dashboard

--- projet_m1_struct/apli_web/interface_web/html/flot.php
+++ projet_m1_struct/apli_web/interface_web/html/flot.php
@@ -160,8 +160,8 @@ if(empty($_SESSION['bassin']) || empty($_SESSION['capteur']) || empty($_SESSION[
                         <!-- /.panel-heading -->
                         <div class="panel-body">
                             <div class="flot-chart">
-                                <div id="toggle" class="flot-chart-content"></div>
-                                <p id="choices" style="float:right; width:135px;"></p>
+                                <div id="toggle" class="flot-chart-content" style="float:left; width:80%;"></div>
+                                <p id="choices" style="float:right; width:20%;"></p>
                             </div>
                         </div>
                         <!-- /.panel-body -->
