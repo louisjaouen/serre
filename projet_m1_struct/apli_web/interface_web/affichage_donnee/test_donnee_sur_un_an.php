e73c4ef1e4912c9d5417abc3adb50be1fab9dcd6
louisjaouen <louisjaouen@me.com>
2017-05-23 17:53:36 +0200

    test nouveau script lecture bdd

--- projet_m1_struct/apli_web/interface_web/affichage_donnee/test_donnee_sur_un_an.php
+++ projet_m1_struct/apli_web/interface_web/affichage_donnee/test_donnee_sur_un_an.php
@@ -38,7 +38,7 @@ function unix_timestamp($date)
 	    die('Erreur : ' . $e->getMessage());
 	}
 
-	$rep = $bdd->query('SELECT * FROM valeur WHERE id_capteur = 1 ORDER BY date_valeur  DESC LIMIT 0,181440000 ');
+	$rep = $bdd->query('SELECT * FROM valeur WHERE id_capteur = 1 ORDER BY date_valeur  DESC LIMIT 0,100 ');
 
 
 	$str="";
