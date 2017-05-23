e9a9ca6807fc026538ef5fb32156947e20afe6bf
louisjaouen <louisjaouen@me.com>
2017-05-23 14:59:12 +0200

    modif dashboard

--- projet_m1_struct/apli_web/interface_web/image_capteur/airtemplvl.php
+++ projet_m1_struct/apli_web/interface_web/image_capteur/airtemplvl.php
@@ -54,13 +54,7 @@
                               <span class="pull-right"><h3 id="tempairtext">température de l'air</h3></span>
                             </div>
                         </div>
-                        <a href="#">
-                            <div class="panel-footer">
-                                <span class="pull-left">View Details</span>
-                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-                                <div class="clearfix"></div>
-                            </div>
-                        </a>
+                        
                     </div>
                 </div>
 </body>
