eac4af68e23974ff292e33aed9d363a18f2880fa
louisjaouen <louisjaouen@me.com>
2017-05-24 11:11:47 +0200

    tentative changement de date

--- projet_m1_struct/apli_web/interface_web/securite/newuser.php
+++ projet_m1_struct/apli_web/interface_web/securite/newuser.php
@@ -4,38 +4,29 @@
 session_start();
 // On teste si la variable de session existe et contient une valeur
 
-if(empty($_SESSION['bassin']) || empty($_SESSION['capteur']) || empty($_SESSION['nombre_de_valeur'])) 
+
+try{
+    // Sous MAMP (Mac)
+    $bdd = new PDO('mysql:host=localhost;dbname=projet;charset=utf8', 'root', 'Louloudu29');
+    }
+catch (Exception $e)
 {
-  $_SESSION['bassin']=1;
-  $_SESSION['capteur']=1;
-  $_SESSION['nombre_de_valeur']=100;
+    die('Erreur : ' . $e->getMessage());
 }
 
-	header('Content-Type: text/html; charset: UTF-8');
-	$bassin=$_GET['b'];
-	$capteur=$_GET['c'];
-	$nombre_de_valeur=$_GET['v'];
-	try{
-        // Sous MAMP (Mac)
-        $bdd = new PDO('mysql:host=localhost;dbname=projet;charset=utf8', 'root', 'Louloudu29');
-        }
-	catch (Exception $e)
-	{
-	    die('Erreur : ' . $e->getMessage());
-	}
 
 
-
-	$message='';
-    if (isset(var)($_POST['nom']) && isset($_POST['prenom'])&& isset($_POST['email'])&& isset($_POST['motdepasse'])&& isset($_POST['telephone']) ) //Oublie d'un champ
-    {
-        $message = 'variable recu';
-    }
-    else //On check le mot de passe
-    {
-        $message = 'variable non recu';
-    }
-    echo $message;
+$message='aaaa';
+echo $message;
+if (isset(var)($_POST['nom']) && isset($_POST['prenom'])&& isset($_POST['email'])&& isset($_POST['motdepasse'])&& isset($_POST['telephone']) ) //Oublie d'un champ
+{
+    $message = 'variable recu';
+}
+else //On check le mot de passe
+{
+    $message = 'variable non recu';
+}
+echo $message;
 
 
 
