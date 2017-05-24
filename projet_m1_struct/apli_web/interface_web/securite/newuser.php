8e6d6e57f5d8b230a6ea30fe9a9dc2202a50b47a
louisjaouen <louisjaouen@me.com>
2017-05-24 11:15:18 +0200

    tentative changement de date

--- projet_m1_struct/apli_web/interface_web/securite/newuser.php
+++ projet_m1_struct/apli_web/interface_web/securite/newuser.php
@@ -1,20 +1,27 @@
 <?php
 
 // On prolonge la session
-//session_start();
+session_start();
 // On teste si la variable de session existe et contient une valeur
 
 
-
-$message='aaaa';
-echo $message;
 if (isset($_POST['nom']) && isset($_POST['prenom'])&& isset($_POST['email'])&& isset($_POST['motdepasse'])&& isset($_POST['telephone']) ) //Oublie d'un champ
 {
-    $message = 'variable recu';
+    $message = '  variable recu';
+    try{
+	        // Sous MAMP (Mac)
+	        $bdd = new PDO('mysql:host=localhost;dbname=projet;charset=utf8', 'root', 'Louloudu29');
+	        }
+	catch (Exception $e)
+	{
+	    die('Erreur : ' . $e->getMessage());
+	}
+
+
 }
 else //On check le mot de passe
 {
-    $message = 'variable non recu';
+    $message = '  variable non recu';
 }
 echo $message;
 
